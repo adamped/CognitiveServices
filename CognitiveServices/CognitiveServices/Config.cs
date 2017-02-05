88679818f7fbf5bc4e358f9d9610d812b8455624
Adam Pedley <adam.pedley@gmail.com>
2017-02-05 12:03:07 +1100

    Initial

---
+++ CognitiveServices/CognitiveServices/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServices
+{
+    public class Config
+    {
+
+        public LUIS LUIS { get; set; }
+
+    }
+
+    public class LUIS
+    {
+        public string Key1 { get; set; }
+        public string Key2 { get; set; }
+    }
+
+}
---
+++ CognitiveServices/CognitiveServices/IntelligentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveServices
+{
+    /// <summary>
+    /// LUIS: Language Understanding Intelligent Service
+    /// https://azure.microsoft.com/en-us/services/cognitive-services/language-understanding-intelligent-service/
+    /// </summary>
+    public class IntelligentService
+    {
+
+        string key1 = "";
+
+    }
+}
